8fe191b12ba977811210a9240b92a47a671df129
Silvio Oliveira <silvio_oliveira17@hotmail.com>
2023-11-18 17:22:36 -0300

    Front-end Silvio

--- velashome/src/main/java/com/velas/velashome/model/entities/Fornecedora.java
+++ velashome/src/main/java/com/velas/velashome/model/entities/Fornecedora.java
@@ -94,5 +94,9 @@ public class Fornecedora {
                 + forEstado + "]";
     }
 
+    public void setVisible(boolean b) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
     
 }
